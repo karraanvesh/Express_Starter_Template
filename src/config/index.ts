c93286dfcdc9c315d250365a080773732640058c
Anvesh <anveshkarra1234@gmail.com>
2026-06-17 12:52:59 +0530

    removed irrelevant folders

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,13 @@ type ServerConfig = {
     PORT : number,
 };
 
+type DBConfig = {
+    DB_NAME : string,
+    DB_USER : string,
+    DB_HOST : string,
+    DB_PASSWORD : string
+}
+
 function loadenv() {
     dotenv.config({debug : true});
     console.log("environment variables loaded");
@@ -14,4 +21,11 @@ console.log("PORT is : " , process.env.PORT);
 
 export const serverConfig : ServerConfig = {
     PORT : (Number(process.env.PORT) || 3001)
+};
+
+export const dbConfig : DBConfig = {
+    DB_NAME : process.env.DB_NAME || 'test_db',
+    DB_USER : process.env.DB_USER || 'root',
+    DB_HOST : process.env.DB_HOST || 'localhost',
+    DB_PASSWORD : process.env.DB_PASSWORD || 'root'
 };
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import v2Router from './routers/v2/index.ts';
 import { genericErrorHandler } from './middlewares/error.middleware.ts';
 import logger from './config/logger.config.ts';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware.ts';
+import sequelize from './db/models/sequelize.ts';
 
 const app = express();
 
@@ -23,5 +24,9 @@ app.use('/api/v2' , v2Router);
 app.use(genericErrorHandler);
  
 app.listen(serverConfig.PORT , async () => {
-    console.log(`Server started at PORT ${serverConfig.PORT}`);
+    logger.info(`Server started at PORT ${serverConfig.PORT}`);
+    logger.info(`Press Ctrl+C to stop the server.`);
+    
+    await sequelize.authenticate(); // Test the connection to the database
+    logger.info('Database connection has been established successfully. ');
 });
